1deceb3323539487ae5b2cda14a1aeb326f5185f
bradytheinventor <bcra1234@gmail.com>
2023-03-06 19:52:19 -0700

    Added version module
    Added a module to keep track of version numbers all in one place

---
+++ src/version.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#Infinite Music Discs version number module
+#Generation tool, datapack design, and resourcepack design by link2_thepast
+
+# app version
+MAJOR = 1
+MINOR = 3
+
+# datapack version
+DP_MAJOR = 2
+DP_MINOR = 0
+
+# legacy datapack version (last 1.x version)
+DP_LEGACY_MAJOR = 1
+DP_LEGACY_MINOR = 9
