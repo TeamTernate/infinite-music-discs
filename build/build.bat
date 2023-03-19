759c993bfba9d1b3b50c99e96858b77855a190ce
bradytheinventor <bcra1234@gmail.com>
2023-03-19 16:57:31 -0700

    Updated windows build script path
    Updated windows build script to have the correct relative paths to files

--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,6 @@
-pyinstaller main.pyw --onefile --clean --noconfirm ^
+pyinstaller ../main.pyw --onefile --clean --noconfirm ^
 --version-file "version.rc" ^
---add-data "data/*;data" ^
+--add-data "../data/*;data" ^
 --name "imd-gui" ^
---icon "data/jukebox_256.ico" ^
---distpath bin
+--icon "../data/jukebox_256.ico" ^
+--distpath "../bin"
