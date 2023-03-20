f6120e2c7fa2e82c8699f73fa994cddcf6872610
bradytheinventor <bcra1234@gmail.com>
2023-03-19 19:09:56 -0700

    Updated windows build workflow
    Windows build is now launched by running "build_win.bat" which launches "build_wrapper_win.bat" which performs all Windows build-related tasks. Currently the only task is to run build.py, but more can be added in the future. "build_win.bat" should have no implementation in it.

--- build/build.bat
+++
@@ -1,6 +0,0 @@
-pyinstaller ../main.pyw --onefile --clean --noconfirm ^
---version-file "version.rc" ^
---add-data "../data/*;data" ^
---name "imd-gui" ^
---icon "../data/jukebox_256.ico" ^
---distpath "../bin"
---
+++ build_win.bat
@@ -0,0 +1,1 @@
+call ./build/build_wrapper_win.bat
