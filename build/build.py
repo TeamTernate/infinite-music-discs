ade529e9faf65677a7793d5788e3becf2adc9886
bradytheinventor <bcra1234@gmail.com>
2023-03-20 19:08:19 -0700

    Updated shebangs
    Updated shebangs for runnable files to use python3.

--- build/build.py
+++ build/build.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#Infinite Music Discs cross-platform build script
+#Script by link2_thepast
+
 import os
 import subprocess
 
--- main.pyw
+++ main.pyw
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 #Infinite Music Discs datapack + resourcepack GUI execution module
