1e4060fced14581ac1a9d2b3863c83aaca1ef386
bradytheinventor <bcra1234@gmail.com>
2023-05-05 10:57:15 -0700

    Write rp files with json dump
    Use json.dump to directly write json-formatted files

--- src/generator/v2.py
+++ src/generator/v2.py
@@ -179,14 +179,13 @@ class GeneratorV2(VirtualGenerator):
         os.makedirs(os.path.join('assets', 'minecraft', 'textures', 'item'))
 
         #write 'pack.mcmeta'
-        with open(os.path.join('pack.mcmeta'),
-                  'w', encoding='utf-8') as pack:
-            pack.write(json.dumps({
+        with open(os.path.join('pack.mcmeta'), 'w', encoding='utf-8') as pack:
+            json.dump({
                 'pack':{
                     'pack_format':pack_format,
                     'description':(Constants.RESOURCEPACK_DESC % len(entry_list.internal_names))
                 }
-            }, indent=4))
+            }, pack, indent=4)
 
         #write 'sounds.json'
         with self.set_directory(os.path.join('assets', 'minecraft')):
@@ -203,7 +202,7 @@ class GeneratorV2(VirtualGenerator):
 
                     json_dict[f'music_disc.{name}'] = sound
 
-                sounds.write(json.dumps(json_dict, indent=4))
+                json.dump(json_dict, sounds, indent=4)
 
     # generate item models
     def write_item_models(self, entry_list: DiscListContents, resourcepack_name: str):
@@ -221,20 +220,20 @@ class GeneratorV2(VirtualGenerator):
                         'model': f'item/music_disc_{entry.internal_name}'
                     })
 
-                music_disc_11.write(json.dumps({
+                json.dump({
                     'parent': 'item/generated',
                     'textures': {'layer0': 'item/music_disc_11'},
                     'overrides': json_list
-                }, indent=4))
+                }, music_disc_11, indent=4)
 
             #write 'music_disc_*.json' files
             for name in entry_list.internal_names:
                 with open(f'music_disc_{name}.json', 'w', encoding='utf-8') as music_disc:
 
-                    music_disc.write(json.dumps({
+                    json.dump({
                         'parent':'item/generated',
                         'textures':{'layer0': f'item/music_disc_{name}'}
-                    }, indent=4))
+                    }, music_disc, indent=4)
 
     # generate assets dir
     def copy_assets(self, entry_list: DiscListContents, resourcepack_name: str):
