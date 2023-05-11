b8b23946ddc712e1a35622e21ab60035b6ed337e
bradytheinventor <bcra1234@gmail.com>
2023-05-11 12:58:16 -0700

    Update stop 11 time
    Increased the "stop disc 11" time a bit to help with multiplayer lag.

--- src/contents/datapack/v2p0.py
+++ src/contents/datapack/v2p0.py
@@ -297,7 +297,7 @@ pre_play = {
 """
 execute store result score @s imd_disc_id run data get block ~ ~ ~ RecordItem.tag.CustomModelData
 function {datapack_name}:play_duration
-scoreboard players set @s imd_stop_11_time 2
+scoreboard players set @s imd_stop_11_time 3
 function {datapack_name}:watchdog_reset_tickcount
 execute as @a[distance=..64] run function {datapack_name}:register_jukebox_listener
 """
