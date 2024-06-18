bb4e062f24514617b30fa19fd63f6f10e842e7fd
bradytheinventor <bcra1234@gmail.com>
2024-06-18 16:50:41 -0700

    Updated creeper loot table and help function
    Help function didn't incorporate changes from datapack v2.2. Creeper loot table was generating in the wrong directory and had some outdated syntax.

--- src/contents/datapack/v3/v3p0.py
+++ src/contents/datapack/v3/v3p0.py
@@ -44,25 +44,37 @@ creeper_music_entry_custom = {
         'function':'minecraft:set_components',
         'components':{
             'minecraft:custom_model_data':'(int){entry.custom_model_data}',
-            'minecraft:hide_additional_tooltip':{},
-            'minecraft:lore':[
-                '{{\"text\":\"{entry.title}\", \"color\":\"gray\", \"italic\":false}}'
-            ]
+            'minecraft:jukebox_playable':{
+                'song':'{pack_name}:{entry.internal_name}'
+            }
         }
     }]
 }
 
 creeper_normal_entries = [
     {
-        'type':'minecraft:item',
-        'functions':[{
-                'function':'minecraft:set_count',
-                'count':{'min':0.0, 'max':2.0, 'type':'minecraft:uniform'}
-            }, {
-                'function':'minecraft:looting_enchant',
-                'count':{'min':0.0, 'max':1.0}
-            }],
-        'name':'minecraft:gunpowder'
+        'type': 'minecraft:item',
+        'functions': [
+        {
+            'add': False,
+            'count': {
+                'type': 'minecraft:uniform',
+                'max': 2.0,
+                'min': 0.0
+            },
+            'function': 'minecraft:set_count'
+        },
+        {
+            'count': {
+                'type': 'minecraft:uniform',
+                'max': 1.0,
+                'min': 0.0
+            },
+            'enchantment': 'minecraft:looting',
+            'function': 'minecraft:enchanted_count_increase'
+        }
+        ],
+        'name': 'minecraft:gunpowder'
     }
 ]
 
@@ -70,7 +82,7 @@ creeper_normal_entries = [
 #   formatting would have been done when the music disc loot table
 #   entries were generated
 creeper_json = {
-    'path': ['data', 'minecraft', 'loot_tables', 'entities', 'creeper.json'],
+    'path': ['data', 'minecraft', 'loot_table', 'entities', 'creeper.json'],
     'repeat': 'single',
     'format_contents': False,
     'contents': \
@@ -81,9 +93,10 @@ creeper_json = {
         {'rolls':1, 'entries':creeper_music_entries, 'conditions':[{
             'condition':'minecraft:entity_properties',
             'predicate':{'type':'#minecraft:skeletons'},
-            'entity':'killer'
+            'entity':'attacker'
         }]
-    }]
+    }],
+    'random_sequence':'minecraft:entities/creeper'
 }
 }
 
@@ -119,16 +132,16 @@ help = {
     'repeat': 'single',
     'contents': \
 """
-tellraw @s {{"text":"\\nInfinite Music Discs Help", "color":"yellow", "bold":"true"}}
-tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"?", "color":"green", "bold":"true"}}, {{"text":"] A datapack & resourcepack that work together to add new music discs to Minecraft. Use ", "color":"gold"}}, {{"text":"the desktop app", "clickEvent":{{"action":"open_url", "value":"https://github.com/TeamTernate/infinite-music-discs"}}, "color":"aqua", "underlined":"true"}}, {{"text":" to generate your own packs and add your own music. Follow the project on ", "color":"gold"}}, {{"text":"CurseForge", "clickEvent":{{"action":"open_url", "value":"https://www.curseforge.com/minecraft/customization/infinite-music-discs"}}, "color":"aqua", "underlined":"true"}}, {{"text":" for update notifications!", "color":"gold"}}]
-tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"!", "color":"red", "bold":"true"}}, {{"text":"]", "color":"gold"}}, {{"text":" Install both the datapack and resourcepack or Infinite Music Discs will not work! The datapack goes in your world's datapack folder, and the resourcepack goes in your resourcepacks folder.", "color":"red"}}]
-tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"i", "color":"aqua", "bold":"true"}}, {{"text":"] To obtain custom discs in survival, get a skeleton to kill a creeper. The creeper will drop a music disc, and there's a chance it will be one of your custom music discs. All discs (including the vanilla discs) have an equal chance to drop. The more discs your pack adds, the harder it will be to get any particular disc. You might consider building a music disc farm.", "color":"gold"}}]
-tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"i", "color":"aqua", "bold":"true"}}, {{"text":"] Give yourself discs in creative with these commands:", "color":"gold"}}]
+tellraw @s {{"text":"\\nInfinite Music Discs Help", "color":"yellow", "bold":true}}
+tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"?", "color":"green", "bold":true}}, {{"text":"] A datapack & resourcepack that work together to add new music discs to Minecraft. Use ", "color":"gold"}}, {{"text":"the desktop app", "clickEvent":{{"action":"open_url", "value":"https://github.com/TeamTernate/infinite-music-discs"}}, "color":"aqua", "underlined":true}}, {{"text":" to generate your own packs and add your own music. Follow the project on ", "color":"gold"}}, {{"text":"CurseForge", "clickEvent":{{"action":"open_url", "value":"https://www.curseforge.com/minecraft/customization/infinite-music-discs"}}, "color":"aqua", "underlined":true}}, {{"text":" for update notifications!", "color":"gold"}}]
+tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"!", "color":"red", "bold":true}}, {{"text":"]", "color":"gold"}}, {{"text":" Install both the datapack and resourcepack or Infinite Music Discs will not work! The datapack goes in your world's datapack folder, and the resourcepack goes in your resourcepacks folder.", "color":"red"}}]
+tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"i", "color":"aqua", "bold":true}}, {{"text":"] To obtain custom discs in survival, get a skeleton to kill a creeper. The creeper will drop a music disc, and there's a chance it will be one of your custom music discs. All discs (including the vanilla discs) have an equal chance to drop. The more discs your pack adds, the harder it will be to get any particular disc. You might consider building a music disc farm.", "color":"gold"}}]
+tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"i", "color":"aqua", "bold":true}}, {{"text":"] Give yourself discs in creative with these commands:", "color":"gold"}}]
 tellraw @s [{{"text":" - ", "color":"gold"}}, {{"text":"/function {pack_name}:give_all_discs", "color":"yellow"}}]
 tellraw @s [{{"text":" - ", "color":"gold"}}, {{"text":"/function {pack_name}:give/<disc name>", "color":"yellow"}}]
-tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"i", "color":"aqua", "bold":"true"}}, {{"text":"] Hoppers and droppers can add/remove custom music discs from jukeboxes!", "color":"gold"}}]
-tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"i", "color":"aqua", "bold":"true"}}, {{"text":"] Is your music playing 'inside your head' instead of from the jukebox? Try checking the \\"{mix_mono_title}\\" setting when you generate your pack. Most music is stereo, and Minecraft plays stereo sounds globally instead of attaching them to a spot in the world.", "color":"gold"}}]
-tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"i", "color":"aqua", "bold":"true"}}, {{"text":"] Still having problems? Use ", "color":"gold"}}, {{"text":"the issue tracker", "clickEvent":{{"action":"open_url", "value":"https://github.com/TeamTernate/infinite-music-discs/issues"}}, "color":"aqua", "underlined":"true"}}, {{"text":" to report bugs or ask for help.", "color":"gold"}}]
+tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"i", "color":"aqua", "bold":true}}, {{"text":"] Hoppers and droppers can add/remove custom music discs from jukeboxes!", "color":"gold"}}]
+tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"i", "color":"aqua", "bold":true}}, {{"text":"] Is your music playing 'inside your head' instead of from the jukebox? Try checking the \\"{mix_mono_title}\\" setting when you generate your pack. Most music is stereo, and Minecraft plays stereo sounds globally instead of attaching them to a spot in the world.", "color":"gold"}}]
+tellraw @s [{{"text":"\\n[", "color":"gold"}}, {{"text":"i", "color":"aqua", "bold":true}}, {{"text":"] Still having problems? Use ", "color":"gold"}}, {{"text":"the issue tracker", "clickEvent":{{"action":"open_url", "value":"https://github.com/TeamTernate/infinite-music-discs/issues"}}, "color":"aqua", "underlined":true}}, {{"text":" to report bugs or ask for help.", "color":"gold"}}]
 """
 }
 
